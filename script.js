1a0aedbcd12da4f00b3576e3dc0a38b9a4d3746e
tom <11286187+Regular-Tom@users.noreply.github.com>
2018-05-18 11:54:10 +1200

    Make long-break system working.

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ var timer = {
     time: 0,
     state: 'stop',
     pomodoro_count: 0,
+    max_pomodoro_count: 4,
     interval_id: null,
 
-    // // Standard Pomodoro time in seconds.
-    // pomodoro_time: 25 * 60,
-    // short_break_time: 5 * 60,
-    // long_break_time: 15 * 60,
-
-    pomodoro_time: 5,
-    short_break_time: 1,
-    long_break_time: 3,
+    // Standard Pomodoro time in seconds.
+    pomodoro_time: 25 * 60,
+    short_break_time: 5 * 60,
+    long_break_time: 15 * 60,
 
     set_time: function(time) {
         this.time = time;
@@ -44,6 +41,9 @@ var timer = {
         }
         else if (state === 'short_break') {
             this.start(this.short_break_time);
+        }
+        else if (state === 'long_break') {
+            this.start(this.long_break_time);
         }
         else if (state === 'stop') {
             this.stop();
@@ -80,9 +80,21 @@ var timer = {
 
     switch_to_next_state: function() {
         if (this.state === 'pomodoro') {
-            this.set_state('short_break');
+            this.set_pomodoro_count(this.pomodoro_count + 1);
+
+            if (this.pomodoro_count < this.max_pomodoro_count) {
+                this.set_state('short_break');
+            }
+            else {
+                this.set_state('long_break')
+            }
         }
         else if (this.state === 'short_break') {
+            this.set_state('pomodoro');
+        }
+        else if (this.state === 'long_break') {
+            this.set_pomodoro_count(0);
+
             this.set_state('pomodoro');
         }
     },
@@ -90,7 +102,9 @@ var timer = {
     set_pomodoro_count: function(count) {
         this.pomodoro_count = count;
 
-        $(`.tomato:lt(${count})`).addClass("fill");
+        // Reset the fill class first for when the count is less than before.
+        $('.tomato').removeClass('fill')
+        $(`.tomato:lt(${count})`).addClass('fill');
     },
 
     // This gets called every second to update the clock.
@@ -99,10 +113,6 @@ var timer = {
             this.set_time(this.time - 1);
         }
         else {
-            if (this.state === 'pomodoro') {
-                this.set_pomodoro_count(this.pomodoro_count + 1);
-            }
-
             this.switch_to_next_state();
         }
     }
@@ -112,5 +122,6 @@ $(document).ready(function() {
     // Assign button press to the functions.
     $('.pomodoro').click(function() {timer.set_state('pomodoro')});
     $('.short-break').click(function() {timer.set_state('short_break')});
+    $('.long-break').click(function() {timer.set_state('long_break')});
     $('.stop').click(function() {timer.set_state('stop')});
 });
